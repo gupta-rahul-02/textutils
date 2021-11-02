d6fdf1547282d3727fd72997e4362acf7a4aa7ea
Rahul Gupta <81256459+rahul-404-gupta@users.noreply.github.com>
2021-11-03 02:09:01 +0530

    More functionalities added to TextForm Module

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,36 +1,53 @@
-import React  , {useState} from 'react'
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const handleUpClick = ()=>{
-       // console.log("Uppercase was clicked" +text);
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked" +text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-    const handleOnChange = (event) =>{
-       // console.log("On change");
-        setText(event.target.value);
-    }
+  const handleOnChange = (event) => {
+    // console.log("On change");
+    setText(event.target.value);
+  };
 
-    const handleLowClick = ()=>{
-        console.log("Lower Case was clicked" + text);
-        let newText = text.toLowerCase();
-        setText(newText);
-    }
-    const [text , setText] = useState('Enter the text here')
-    //text = "nex text"; wronng way to change state
-    //setText("new text"); // correct way to change the state
-    return (
+  const handleLowClick = () => {
+    console.log("Lower Case was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const [text, setText] = useState("");
+  //text = "nex text"; wronng way to change state
+  //setText("new text"); // correct way to change the state
+  return (
+    <>
     <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-        </div>
-        <button className=" btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-        
-        <button className="btn btn-primary " onClick={handleLowClick}>Convet to Lowercase</button>
-    
-        
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="8"
+        ></textarea>
+      </div>
+      <button className=" btn btn-primary mx-2" onClick={handleUpClick}>
+        Convert to Uppercase
+      </button>
+      <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+        Convet to Lowercase
+      </button>
     </div>
-    )
+
+    <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length-1} words and {text.length} characters</p>
+        <p>Minimum time reqquired to read the text :- {0.008 * text.split(" ").length} Minutes</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+  );
 }
