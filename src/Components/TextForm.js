91f92f1035417bb5278fa4f6a7c41665121bb541
Rahul Gupta <81256459+rahul-404-gupta@users.noreply.github.com>
2021-11-03 02:35:54 +0530

    Email extractracting function added

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -13,10 +13,24 @@ export default function TextForm(props) {
   };
 
   const handleLowClick = () => {
-    console.log("Lower Case was clicked" + text);
+    //console.log("Lower Case was clicked" + text);
     let newText = text.toLowerCase();
     setText(newText);
   };
+
+  const handleClearClick = () => {
+    let newText ='';
+    setText(newText);
+    setEmail(newText);
+  };
+
+  const handleEmailClick = () => {
+    let newText = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)+" ";;
+    console.log(newText);
+    setEmail(newText);
+  };
+
+  const [email, setEmail] = useState("");
   const [text, setText] = useState("");
   //text = "nex text"; wronng way to change state
   //setText("new text"); // correct way to change the state
@@ -39,14 +53,22 @@ export default function TextForm(props) {
       <button className="btn btn-primary mx-2" onClick={handleLowClick}>
         Convet to Lowercase
       </button>
+      <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+        Clear Text
+      </button>
+      <button className="btn btn-primary mx-2" onClick={handleEmailClick}>
+        Get Email
+      </button>
     </div>
 
     <div className="container my-3">
         <h2>Your text summary</h2>
         <p>{text.split(" ").length-1} words and {text.length} characters</p>
-        <p>Minimum time reqquired to read the text :- {0.008 * text.split(" ").length} Minutes</p>
+        <p>Minimum time reqquired to read the text :- {0.008 * (text.split(" ").length-1)} Minutes</p>
         <h2>Preview</h2>
         <p>{text}</p>
+        <h2>Emails Found</h2>
+        <p>{email}</p>
     </div>
     </>
   );
